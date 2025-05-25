d7d737c9c5859f5f2c9ad20e1541a31f55ea6036
RConant6 <rconant6@gmail.com>
2025-05-25 16:16:56 -0400

    [ecs] added for toplevel API exposure

---
+++ game/ecs.zig
@@ -0,0 +1,7 @@
+const types = @import("ecs/types.zig");
+
+pub const Entity = types.Entity;
+pub const ComponentTag = types.ComponentTag;
+pub const ComponentType = types.ComponentType;
+pub const EntityManager = types.EntityManager;
+pub const TransformComp = types.TransformComp;
