c48ac7765721a3507238fa6dc705abc745212c03
RConant6 <rconant6@gmail.com>
2025-07-11 21:49:39 -0400

    moved resources, nuked old types.zig files

--- engine/src/assets/types.zig
+++
@@ -1,5 +0,0 @@
-pub const AssetManager = @import("assetManager.zig").AssetManager;
-
-const fMgr = @import("fontManager.zig");
-pub const FontManager = fMgr.FontManager;
-pub const Font = fMgr.Font;
